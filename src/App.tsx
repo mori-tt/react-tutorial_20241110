8bd0dd0d6c290b82308be6016aa085e8d54f1d57
T_Mori <morit0301@gmail.com>
2024-11-10 17:45:08 +0900

    Header/ Footerの追加 - Header コンポーネントの追加 - Footer コンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
+      <Header />
       <Hero />
       <About />
+      <Footer />
     </>
   );
 }
